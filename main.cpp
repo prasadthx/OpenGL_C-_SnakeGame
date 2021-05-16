9312886a18b010a8bb6c685cb9e6b1d6bc6e0391
prasad <prasadzore31@gmail.com>
2021-05-16 21:38:24 +0530

    Libraries linked

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <GL/gl.h>
+#include <GL/glut.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+void displayCallback();
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE);
+    glutInitWindowSize(500, 500);
+    glutCreateWindow("SNAKE GAME");
+    glutDisplayFunc(displayCallback);
+    glutMainLoop();
     return 0;
+}
+
+void displayCallback(){
+    glutSwapBuffers();
 }
